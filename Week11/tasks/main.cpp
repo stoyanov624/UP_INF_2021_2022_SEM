ed2be227e1d9c4bf7aeeb0e0970924651551d61c
Ivo <istoyanov@astea.net>
2021-12-16 17:16:13 +0200

    tasks

---
+++ Week11/tasks/main.cpp
@@ -0,0 +1,195 @@
+#include <iostream>
+
+using namespace std;
+
+void initMatrix(int** matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    cout << endl;
+}   
+
+void printMatrix(int** matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int getShooterColPosition(int** matrix, int shooterRowPosition, int cols) {
+    for (int i = 0; i < cols; i++) {
+        if(matrix[shooterRowPosition][i] == 3) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void shootBuilding(int** matrix, int rows, int cols) {
+    int shooterRowPosition = rows - 1;
+    int shooterColPosition = getShooterColPosition(matrix, shooterRowPosition, cols);
+
+    if(shooterColPosition == -1) {
+        cout << "404: Traycho not found!\n";
+        return;
+    }
+
+    for (int i = shooterRowPosition; i >= 0; i--) {
+        if(matrix[i][shooterColPosition] == 2) {
+             cout << "Hit a window\n";
+             return;
+        }
+
+        if(matrix[i][shooterColPosition] == 1) {
+            cout << "Hit a wall\n";
+            return;
+        }
+
+        if(i == 0) {
+            cout << "Over building\n";
+            return;
+        }
+        shooterColPosition++;
+    }
+}
+
+void spreadUpperRight(int** matrix, int rows, int cols, int dustRowPosition, int dustColPosition) {
+    if(dustColPosition == cols - 1 || dustRowPosition == 0) {
+        return;
+    }
+    int dustValue = matrix[dustRowPosition][dustColPosition];
+
+    for (int j = dustColPosition + 1, i = dustRowPosition - 1; ;j++, i--) {
+        if(i == 0) {
+            matrix[i][j] += dustValue;
+            return;
+        }
+
+        if(j == cols - 1) {
+            matrix[i][j] += dustValue;
+            return;
+        }
+        matrix[i][j] += dustValue;
+    } // може да се напише и с while
+    
+}
+
+void spreadUpperLeft(int** matrix, int rows, int cols, int dustRowPosition, int dustColPosition) {
+    if(dustRowPosition == 0 || dustColPosition == 0) {
+        return;
+    }
+
+    int dustValue = matrix[dustRowPosition][dustColPosition];
+
+    for (int j = dustColPosition - 1, i = dustRowPosition - 1; ;j--, i--) {
+        if(i == 0) {
+            matrix[i][j] += dustValue;
+            return;
+        }
+
+        if(j == 0) {
+            matrix[i][j] += dustValue;
+            return;
+        }
+        matrix[i][j] += dustValue;
+    } // може да се напише и с while
+}
+
+void spreadDownRight(int** matrix, int rows, int cols, int dustRowPosition, int dustColPosition) {
+    if(dustRowPosition == rows - 1 || dustColPosition == cols - 1) {
+        return;
+    }
+
+    int dustValue = matrix[dustRowPosition][dustColPosition];
+
+    for (int j = dustColPosition + 1, i = dustRowPosition + 1; ;j++, i++) {
+        if(i == rows - 1) {
+            matrix[i][j] += dustValue;
+            return;
+        }
+
+        if(j == cols - 1) {
+            matrix[i][j] += dustValue;
+            return;
+        }
+        matrix[i][j] += dustValue;
+    } // може да се напише и с while
+}
+
+void spreadDownLeft(int** matrix, int rows, int cols, int dustRowPosition, int dustColPosition) {
+    if(dustRowPosition == rows - 1 || dustColPosition == 0) {
+        return;
+    }
+
+    int dustValue = matrix[dustRowPosition][dustColPosition];
+
+    for (int j = dustColPosition - 1, i = dustRowPosition + 1; ;j--, i++) {
+        if(i == rows - 1) {
+            matrix[i][j] += dustValue;
+            return;
+        }
+
+        if(j == 0) {
+            matrix[i][j] += dustValue;
+            return;
+        }
+        matrix[i][j] += dustValue;
+    } // може да се напише и с while
+}
+
+void spreadIce(int** matrix, int rows, int cols, int dustRowPosition, int dustColPosition) {
+    spreadUpperRight(matrix, rows, cols, dustRowPosition, dustColPosition);
+    spreadUpperLeft(matrix, rows, cols, dustRowPosition, dustColPosition);
+    spreadDownRight(matrix, rows, cols, dustRowPosition, dustColPosition);
+    spreadDownLeft(matrix, rows, cols, dustRowPosition, dustColPosition);
+}
+
+void generateSnowflakes(int** matrix, int rows, int cols) {
+    int previusValue = 0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if(matrix[i][j] != 0 && previusValue - matrix[i][j] != 0) {
+                previusValue = matrix[i][j];
+                spreadIce(matrix, rows, cols, i, j);
+            }
+        }
+    }
+}
+
+
+int fact(int n) {
+    if(n < 0) {
+        return -1;
+    }
+
+    if(n == 0) {
+        return 1;
+    }   
+    cout << "Recursion";
+    return n * fact(n - 1);
+}
+
+int main() {
+    // int rows = 5;
+    // int cols = 5;
+    // int** matrix = new int*[rows];
+
+    // for (size_t i = 0; i < rows; i++) {
+    //     matrix[i] = new int[cols];
+    // }
+
+    // initMatrix(matrix, rows, cols);
+
+    // generateSnowflakes(matrix, rows, cols);
+    // printMatrix(matrix, rows, cols);
+
+    int result = fact(-1);
+
+    cout << result;
+}
